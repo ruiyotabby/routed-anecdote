b1c4a1af524adb50901d2a2db9ddd2c081e82800
Ruiyot Abby <ruiyotabby@gmail.com>
2023-10-07 12:07:56 +0300

    Add custome hook to the anecdote creation form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Route, Link, Routes, useNavigate, useMatch } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -52,17 +53,17 @@ const Footer = () => {
 }
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -73,15 +74,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
---
+++ src/hooks/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export const useField = (name) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => setValue(event.target.value)
+
+  return {
+    name,
+    value,
+    onChange
+  }
+}
